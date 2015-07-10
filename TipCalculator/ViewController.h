81077463400d08cbf2e9295fc7673fa7d20c36b8
Auston <auston.salvana@live.com>
2015-07-10 10:37:34 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Auston Salvana on 7/10/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
